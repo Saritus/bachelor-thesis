5a06113b87b477d6468f454dd7b715575440c3e4
Sebastian Mischke <SebastianMischke@gmx.de>
2017-03-07 10:57:36 +0100

    Show image

--- combineImages.py
+++ combineImages.py
@@ -1,7 +1,10 @@
 from PIL import Image
+from pylab import imshow, show, cm, imsave
 
 images = []
 images.extend([Image.open("samples/1.png").convert('RGB')])
 images.extend([Image.open("samples/2.png").convert('RGB')])
 images.extend([Image.open("samples/3.png").convert('RGB')])
 images.extend([Image.open("samples/4.png").convert('RGB')])
+imshow(images[0])
+show()
