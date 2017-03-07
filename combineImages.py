f6d21a35e3d7a28aa60daeedb8161ce521411b12
Sebastian Mischke <SebastianMischke@gmx.de>
2017-03-07 11:32:56 +0100

    Move combine_image to seperate function

--- combineImages.py
+++ combineImages.py
@@ -1,16 +1,20 @@
 from PIL import Image
 from pylab import imshow, show, cm, imsave
 
+def combine_images(images):
+    output = Image.new("RGB", (100, 100))
+    output.paste(images[0], (0,0,50,50))
+    output.paste(images[1], (50,0,100,50))
+    output.paste(images[2], (0,50,50,100))
+    output.paste(images[3], (50,50,100,100))
+    return output
+
 images = []
 images.extend([Image.open("samples/1.png").convert('RGB')])
 images.extend([Image.open("samples/2.png").convert('RGB')])
 images.extend([Image.open("samples/3.png").convert('RGB')])
 images.extend([Image.open("samples/4.png").convert('RGB')])
 
-output = Image.new("RGB", (100, 100))
-output.paste(images[0], (0,0,50,50))
-output.paste(images[1], (50,0,100,50))
-output.paste(images[2], (0,50,50,100))
-output.paste(images[3], (50,50,100,100))
+output = combine_images(images)
 imshow(output)
 show()
