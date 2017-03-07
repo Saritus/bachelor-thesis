d26d90ec9973a17295cadcf989f442c6050c3c04
Sebastian Mischke <SebastianMischke@gmx.de>
2017-03-07 18:20:48 +0100

    Show map of all adresses

--- loadcsv.py
+++ loadcsv.py
@@ -1,5 +1,14 @@
 import csv
 with open('data/Gebaeude_Dresden.csv') as csvfile:
     reader = csv.DictReader(csvfile, delimiter='\t')
+    xcoords = []
+    ycoords = []
+    
     for row in reader:
-        print(row["City"])
+        
+        xcoords.extend([float(row['SP_GEOMETRY_Center'].replace(',','.'))])
+        ycoords.extend([float(row['X_Coordinate'].replace(',','.'))])
+
+import matplotlib.pyplot as plt
+plt.plot(xcoords, ycoords, 'bo', markersize=0.5)
+plt.show()
