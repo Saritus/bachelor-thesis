d5b7bcc7a16e44a41000173f57bbf856cb3b4e06
Sebastian Mischke <SebastianMischke@gmx.de>
2017-03-07 12:04:58 +0100

    Calculate the size based on the input images

--- combineImages.py
+++ combineImages.py
@@ -10,13 +10,11 @@ def combine_images(images):
         raise ValueError("Width of image 2 and 4 are different")
     if(images[2].size[1] is not images[3].size[1]):
         raise ValueError("Height of image 3 and 4 are different")
-    width = images[0].size[0]
-    height = images[0].size[1]
-    output = Image.new("RGB", (width*2, height*2))
+    output = Image.new("RGB", (images[0].size[0]+images[1].size[0], images[0].size[1]+images[2].size[1]))
     output.paste(images[0], (0,0))
-    output.paste(images[1], (width,0))
-    output.paste(images[2], (0,height))
-    output.paste(images[3], (width, height))
+    output.paste(images[1], (images[0].size[0],0))
+    output.paste(images[2], (0,images[0].size[1]))
+    output.paste(images[3], (images[0].size[0], images[0].size[1]))
     return output
 
 images = []
