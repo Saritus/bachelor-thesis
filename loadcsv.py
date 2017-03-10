abc708b28da5e94e2a2af6807059cd3e51a33015
Sebastian Mischke <SebastianMischke@gmx.de>
2017-03-10 19:21:42 +0100

    Plot map with flags as color

--- loadcsv.py
+++ loadcsv.py
@@ -7,12 +7,11 @@ with open('data/Gebaeude_Dresden.csv') as csvfile:
     flags = []
     
     for row in reader:
-        
         xcoords.extend([float(row['X_Coordinate'].replace(',','.'))])
         ycoords.extend([float(row['Y_Coordinate'].replace(',','.'))])
         zipcodes.extend([float(row['ZipCode'])])
         flags.extend([float(row['Flag_Coordinates'])])
 
 import matplotlib.pyplot as plt
-plt.plot(xcoords, ycoords, 'bo', markersize=0.5)
+plt.scatter(xcoords, ycoords, c=flags, s=0.5, cmap='jet')
 plt.show()
