490ec9752edb5e702f39e621a5cf6fb1ef39856d
Sebastian Mischke <SebastianMischke@gmx.de>
2017-03-10 19:19:48 +0100

    Load zipcodes from csv

--- loadcsv.py
+++ loadcsv.py
@@ -3,11 +3,13 @@ with open('data/Gebaeude_Dresden.csv') as csvfile:
     reader = csv.DictReader(csvfile, delimiter='\t')
     xcoords = []
     ycoords = []
+    zipcodes = []
     
     for row in reader:
         
         xcoords.extend([float(row['X_Coordinate'].replace(',','.'))])
         ycoords.extend([float(row['Y_Coordinate'].replace(',','.'))])
+        zipcodes.extend([float(row['ZipCode'])])
 
 import matplotlib.pyplot as plt
 plt.plot(xcoords, ycoords, 'bo', markersize=0.5)
