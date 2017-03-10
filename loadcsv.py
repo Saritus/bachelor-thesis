ec03ea5b5c52bdf519dcc45bfe1b1e50d7e15bdb
Sebastian Mischke <SebastianMischke@gmx.de>
2017-03-10 19:20:02 +0100

    Load flags from csv

--- loadcsv.py
+++ loadcsv.py
@@ -4,12 +4,14 @@ with open('data/Gebaeude_Dresden.csv') as csvfile:
     xcoords = []
     ycoords = []
     zipcodes = []
+    flags = []
     
     for row in reader:
         
         xcoords.extend([float(row['X_Coordinate'].replace(',','.'))])
         ycoords.extend([float(row['Y_Coordinate'].replace(',','.'))])
         zipcodes.extend([float(row['ZipCode'])])
+        flags.extend([float(row['Flag_Coordinates'])])
 
 import matplotlib.pyplot as plt
 plt.plot(xcoords, ycoords, 'bo', markersize=0.5)
