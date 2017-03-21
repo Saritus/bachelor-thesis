e407af86ffce57c6719ea32a8a61eba34b347842
Sebastian Mischke <SebastianMischke@gmx.de>
2017-03-21 01:11:32 +0100

    Formatting

--- keras-image-classification.py
+++ keras-image-classification.py
@@ -1,49 +1,48 @@
-from keras.datasets import mnist 
+from keras.datasets import mnist
+
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
 from keras.models import Sequential
 from keras.layers import (Activation, Dropout, Flatten, Dense, Convolution2D, MaxPooling2D)
- 
- 
+
 ## Some model and data processing constants
- 
+
 batch_size = 128
 nb_classes = 10
 nb_epoch = 12
- 
+
 # input image dimensions
 img_rows, img_cols = 28, 28
- 
+
 # number of convolutional filters to use
 nb_filters = 32
 # size of pooling area for max pooling
 nb_pool = 2
 # convolution kernel size
 nb_conv = 3
- 
- 
+
 model = Sequential()
 
 model.add(Convolution2D(nb_filters, (nb_conv, nb_conv), border_mode='same', input_shape=(1, img_rows, img_cols)))
 model.add(Activation('relu'))
-#model.add(Convolution2D(nb_filters, nb_conv, nb_conv))
-#model.add(Activation('relu'))
-#model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
+# model.add(Convolution2D(nb_filters, nb_conv, nb_conv))
+# model.add(Activation('relu'))
+# model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
 model.add(Dropout(0.25))
- 
+
 model.add(Flatten())
 model.add(Dense(128))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
 model.add(Dense(nb_classes))
 model.add(Activation('softmax'))
- 
+
 model.compile(loss='categorical_crossentropy',
               optimizer='adadelta',
               metrics=['accuracy'])
 
 from keras.utils import np_utils
- 
+
 X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
 X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
 X_train = X_train.astype('float32')
@@ -53,20 +52,20 @@ X_test /= 255
 print('X_train shape:', X_train.shape)
 print(X_train.shape[0], 'train samples')
 print(X_test.shape[0], 'test samples')
- 
+
 # convert class vectors to binary class matrices
 Y_train = np_utils.to_categorical(y_train, nb_classes)
 Y_test = np_utils.to_categorical(y_test, nb_classes)
 
-history = model.fit(X_train, Y_train,  		
-          batch_size=batch_size, 
-          epochs=nb_epoch, verbose=2,	
-          validation_data=(X_test, Y_test))
+history = model.fit(X_train, Y_train,
+                    batch_size=batch_size,
+                    epochs=nb_epoch, verbose=2,
+                    validation_data=(X_test, Y_test))
 
 print(history.history.keys())
 import matplotlib.pyplot as plt
 
-#ACC VS VAL_ACC
+# ACC VS VAL_ACC
 plt.plot(history.history['acc'])
 plt.plot(history.history['val_acc'])
 plt.title('model accuracy ACC VS VAL_ACC')
@@ -74,8 +73,8 @@ plt.ylabel('accuracy')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
 plt.show()
-# summarize history for loss
-#LOSS VS VAL_LOSS
+
+# LOSS VS VAL_LOSS
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
 plt.title('model loss LOSS vs VAL_LOSS')
