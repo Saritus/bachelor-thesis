ce691f8a6e86e6e906e66d59536c478fcc638ff4
Sebastian Mischke <SebastianMischke@gmx.de>
2017-03-21 05:43:20 +0100

    Load and show teach array

--- result/print_prediction.py
+++ result/print_prediction.py
@@ -1,8 +1,10 @@
 def show_prediction_from_file(filename):
     import cPickle as pickle
-    x, y, prediction = pickle.load(open(filename, "rb"))
+    x, y, t, prediction = pickle.load(open(filename, "rb"))
 
     import matplotlib.pyplot as plt
+    plt.scatter(x, y, c=t, s=0.5, cmap='jet')
+    plt.show()
     plt.scatter(x, y, c=prediction, s=0.5, cmap='jet')
     plt.show()
     
