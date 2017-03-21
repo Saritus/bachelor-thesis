ba4cd62ea93a9b7ca4ad8c96d366a46e57106681
Sebastian Mischke <SebastianMischke@gmx.de>
2017-03-21 01:29:07 +0100

    Extract graphs to methods

--- keras-image-classification.py
+++ keras-image-classification.py
@@ -73,23 +73,32 @@ history = model.fit(X_train, Y_train,
                     epochs=nb_epoch, verbose=2,
                     validation_data=(X_test, Y_test))
 
-print(history.history.keys())
-import matplotlib.pyplot as plt
 
-# ACC VS VAL_ACC
-plt.plot(history.history['acc'])
-plt.plot(history.history['val_acc'])
-plt.title('model accuracy ACC VS VAL_ACC')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
+def show_acc():
+    # ACC VS VAL_ACC
+    import matplotlib.pyplot as plt
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('model accuracy ACC VS VAL_ACC')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
 
-# LOSS VS VAL_LOSS
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('model loss LOSS vs VAL_LOSS')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
+
+show_acc()
+
+
+def show_loss():
+    # LOSS VS VAL_LOSS
+    import matplotlib.pyplot as plt
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss LOSS vs VAL_LOSS')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+
+show_loss()
