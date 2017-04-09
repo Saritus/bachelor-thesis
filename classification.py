43165706bc60084a5549d6defd315d6d2a6a6610
Sebastian Mischke <SebastianMischke@gmx.de>
2017-04-10 01:36:48 +0200

    Move center_crop_image to functions.py

--- classification.py
+++ classification.py
@@ -1,15 +1,4 @@
-def center_crop_image(path, new_width, new_height):
-    from PIL import Image
-
-    im = Image.open(path).convert('RGB')
-    width, height = im.size  # Get dimensions
-
-    left = (width - new_width) / 2
-    top = (height - new_height) / 2
-    right = (width + new_width) / 2
-    bottom = (height + new_height) / 2
-
-    return im.crop((left, top, right, bottom))
+from functions import center_crop_image
 
 
 def ensure_dir(filepath):
--- functions.py
+++ functions.py
@@ -1,3 +1,17 @@
+def center_crop_image(path, new_width, new_height):
+    from PIL import Image
+
+    im = Image.open(path).convert('RGB')
+    width, height = im.size  # Get dimensions
+
+    left = (width - new_width) / 2
+    top = (height - new_height) / 2
+    right = (width + new_width) / 2
+    bottom = (height + new_height) / 2
+
+    return im.crop((left, top, right, bottom))
+
+
 def main():
     return
 
