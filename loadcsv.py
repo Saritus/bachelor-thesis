ee2adb87f87790fc39c5720fe30a4e32bf55d0d3
Sebastian Mischke <SebastianMischke@gmx.de>
2017-04-10 01:40:14 +0200

    Move load_csv to functions.py

--- loadcsv.py
+++ loadcsv.py
@@ -1,3 +1,72 @@
+from functions import center_crop_image, ensure_dir, download_image
+
+def load_csv(filename):
+    X_meta = []
+    Y_train = []
+    X_images = []
+    Coordinates = []
+
+    import csv
+    csvfile = open(filename)
+    reader = csv.DictReader(csvfile, delimiter='\t')
+
+    count = 0
+    for row in reader:
+        if count > 10000:
+            break
+        count += 1
+
+        filepath = "nwt-data/images/" + row['ZipCode'].zfill(5) + "/" + row['House_ID'] + ".jpg"
+        ensure_dir(filepath)
+
+        # Fill image input array
+        from scipy import misc
+        try:
+            img = misc.imread(filepath)
+        except IOError:
+            # Download image from GoogleMaps API
+            download_image(filepath, row)
+            print count
+            img = misc.imread(filepath)
+        X_images.extend([img])
+
+        # Fill coordinates array
+        coordinate = [
+            float(row['X_Coordinate'].replace(',', '.')),
+            float(row['Y_Coordinate'].replace(',', '.')),
+        ]
+        Coordinates.extend([coordinate])
+
+        # Fill metadata input array
+        x = [
+            # float(row['X_Coordinate'].replace(',', '.')),
+            # float(row['Y_Coordinate'].replace(',', '.')),
+            float(hash(row['District'])) % 100,
+            float(hash(row['Street'])) % 100,
+            # float(row['ZipCode']),
+        ]
+        X_meta.extend([x])
+
+        # Fill output array
+        y = [
+            int(row['ZipCode']),
+        ]
+        Y_train.extend([y])
+
+    import numpy
+
+    Coordinates = numpy.asarray(Coordinates, dtype=numpy.float32)
+    X_meta = numpy.asarray(X_meta, dtype=numpy.float32)
+    X_images = numpy.asarray(X_images, dtype=numpy.float32)
+    Y_train = numpy.asarray(Y_train, dtype=numpy.float32)
+    X_train = [X_images, X_meta]
+
+    print ("X_images.shape", X_images.shape)
+    print ("Y_train.shape", Y_train.shape)
+
+    return Coordinates, (X_train, Y_train)
+
+
 def main():
     import csv
 
