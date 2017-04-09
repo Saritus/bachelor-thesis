2d7d6f29f5a7667a7ce5ae91dac0a7ed687fbd5a
Sebastian Mischke <SebastianMischke@gmx.de>
2017-04-10 01:24:31 +0200

    Move everything into main

--- loadcsv.py
+++ loadcsv.py
@@ -1,26 +1,23 @@
-import csv
-
-with open('nwt-data/Gebaeude_Dresden.csv') as csvfile:
-    reader = csv.DictReader(csvfile, delimiter='\t')
-    xcoords = []
-    ycoords = []
-    zipcodes = []
-    flags = []
-
-    for row in reader:
-        xcoords.extend([float(row['X_Coordinate'].replace(',', '.'))])
-        ycoords.extend([float(row['Y_Coordinate'].replace(',', '.'))])
-        zipcodes.extend([float(hash(row['ZipCode'])) % 17])
-        flags.extend([float(row['Flag_Coordinates'])])
-
-import matplotlib.pyplot as plt
-
-plt.scatter(xcoords, ycoords, c=zipcodes, s=0.5, cmap='jet')
-plt.show()
-
-
 def main():
-    pass
+    import csv
+
+    with open('nwt-data/Gebaeude_Dresden.csv') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter='\t')
+        xcoords = []
+        ycoords = []
+        zipcodes = []
+        flags = []
+
+        for row in reader:
+            xcoords.extend([float(row['X_Coordinate'].replace(',', '.'))])
+            ycoords.extend([float(row['Y_Coordinate'].replace(',', '.'))])
+            zipcodes.extend([float(hash(row['ZipCode'])) % 17])
+            flags.extend([float(row['Flag_Coordinates'])])
+
+    import matplotlib.pyplot as plt
+
+    plt.scatter(xcoords, ycoords, c=zipcodes, s=0.5, cmap='jet')
+    plt.show()
 
 
 if __name__ == "__main__":
