68f582d62b3192d8ac1ed973e128f96393464218
Sebastian Mischke <SebastianMischke@gmx.de>
2017-04-19 00:46:24 +0200

    Add comments

--- training_data_generator.py
+++ training_data_generator.py
@@ -10,15 +10,18 @@ from PIL import Image, ImageTk
 
 class App:
     def __init__(self, master):
+        # Frame
         frame = Frame(master)
         frame.pack()
 
+        # Image
         self.canvas = Canvas(root, width=598, height=598)
         self.canvas.pack(side=BOTTOM)
         self.pilImage = Image.open("nwt-data/google-images/01067/100000010.jpg")
         self.image = ImageTk.PhotoImage(self.pilImage)
         self.imagesprite = self.canvas.create_image(299, 299, image=self.image)
 
+        # Buttons
         self.button = Button(frame, text="YES", command=self.yes, width=10)
         self.button.pack(side=LEFT)
         self.slogan = Button(frame, text="NO", command=self.no, width=10)
@@ -26,9 +29,11 @@ class App:
 
     def yes(self):
         print("YES")
+        self.change_image("nwt-data/google-images/01067/100000182.jpg")
 
     def no(self):
         print("NO")
+        self.change_image("nwt-data/google-images/01067/100000291.jpg")
 
     def change_image(self, filepath):
         self.pilImage = Image.open(filepath)
