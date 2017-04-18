1099dd3801f3d4338c95a468d62e9d8ef1dc6831
Sebastian Mischke <SebastianMischke@gmx.de>
2017-04-19 00:30:33 +0200

    Add image to interface

--- training_data_generator.py
+++ training_data_generator.py
@@ -5,12 +5,20 @@ csvfile = open('nwt-data/Gebaeude_Dresden.csv')
 reader = csv.DictReader(csvfile, delimiter='\t')
 
 from Tkinter import *
+from PIL import Image, ImageTk
 
 
 class App:
     def __init__(self, master):
         frame = Frame(master)
         frame.pack()
+
+        self.canvas = Canvas(root, width=598, height=598)
+        self.canvas.pack(side=BOTTOM)
+        self.pilImage = Image.open("nwt-data/google-images/01067/100000010.jpg")
+        self.image = ImageTk.PhotoImage(self.pilImage)
+        self.imagesprite = self.canvas.create_image(299, 299, image=self.image)
+
         self.button = Button(frame, text="QUIT", command=quit)
         self.button.pack(side=LEFT)
         self.slogan = Button(frame, text="Hello", command=self.write_slogan)
