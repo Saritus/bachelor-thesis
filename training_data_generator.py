b3e13fd2084e3da46b9001db25a6fa780099dcee
Sebastian Mischke <SebastianMischke@gmx.de>
2017-04-19 00:03:57 +0200

    Add tkinter window example

--- training_data_generator.py
+++ training_data_generator.py
@@ -3,3 +3,23 @@ import csv
 
 csvfile = open('nwt-data/Gebaeude_Dresden.csv')
 reader = csv.DictReader(csvfile, delimiter='\t')
+
+from Tkinter import *
+
+
+class App:
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+        self.button = Button(frame, text="QUIT", command=quit)
+        self.button.pack(side=LEFT)
+        self.slogan = Button(frame, text="Hello", command=self.write_slogan)
+        self.slogan.pack(side=LEFT)
+
+    def write_slogan(self):
+        print("Tkinter is easy to use!")
+
+
+root = Tk()
+app = App(root)
+root.mainloop()
