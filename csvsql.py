a560f76362552a56fb9fc61168555082357a9c62
Sebastian Mischke <SebastianMischke@gmx.de>
2017-05-07 20:34:54 +0200

    Change csv file

--- csvsql.py
+++ csvsql.py
@@ -41,7 +41,7 @@ def convert_csv_to_sql(csvfile, sqlfile):
 
 
 def main():
-    convert_csv_to_sql("nwt-data/Gebaeude_Dresden.csv", "nwt-data/Gebaeude_Dresden.db")
+    convert_csv_to_sql("nwt-data/Gebaeude_Dresden_shuffle.csv", "nwt-data/Gebaeude_Dresden.db")
     return
 
 
