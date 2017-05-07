902915e06fb04c8176521750a8310a07fe5bb8dc
Sebastian Mischke <SebastianMischke@gmx.de>
2017-05-07 20:09:49 +0200

    Add basic python structure

---
+++ csvsql.py
@@ -0,0 +1,11 @@
+def convert_csv_to_sql(csvfile, sqlfile):
+    return
+
+
+def main():
+    convert_csv_to_sql("nwt-data/Gebaeude_Dresden.csv", "nwt-data/Gebaeude_Dresden.db")
+    return
+
+
+if __name__ == "__main__":
+    main()
