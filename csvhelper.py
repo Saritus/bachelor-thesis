c21547da8ea6af15707365e8d4adfae3a71afd96
Sebastian Mischke <SebastianMischke@gmx.de>
2017-05-11 20:45:40 +0200

    Change main

--- csvhelper.py
+++ csvhelper.py
@@ -101,25 +101,10 @@ def load_csv(filename):
 
 
 def main():
-    load_csv("nwt-data/Gebaeude_Dresden_shuffle.csv")
+    reader = csvReader("nwt-data/Gebaeude_Dresden_shuffle.csv")
+    writer = csvWriter("nwt-data/Output.csv", reader.fieldnames)
 
-    import csv
-
-    with open('nwt-data/Gebaeude_Dresden.csv') as csvfile:
-        reader = csv.DictReader(csvfile, delimiter='\t')
-        xcoords = []
-        ycoords = []
-        zipcodes = []
-        flags = []
-
-        for row in reader:
-            xcoords.extend([float(row['X_Coordinate'].replace(',', '.'))])
-            ycoords.extend([float(row['Y_Coordinate'].replace(',', '.'))])
-            zipcodes.extend([float(hash(row['ZipCode'])) % 17])
-            flags.extend([float(row['Flag_Coordinates'])])
-
-    from visualize import point_plot
-    point_plot(xcoords, ycoords, zipcodes)
+    # load_csv("nwt-data/Gebaeude_Dresden_shuffle.csv")
 
 
 if __name__ == "__main__":
