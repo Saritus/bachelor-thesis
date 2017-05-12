2b5320919c634396dfbad867aef67aad0e3b1518
Sebastian Mischke <SebastianMischke@gmx.de>
2017-05-12 18:46:10 +0200

    Add get_column

--- csvhelper.py
+++ csvhelper.py
@@ -13,6 +13,12 @@ class csvReader:
         row = self.reader.next()
         return row
 
+    def get_column(self, name):
+        column = []
+        for row in self.reader:
+            column.extend([row[name]])
+        return column
+
     @property
     def fieldnames(self):
         return self.reader.fieldnames
