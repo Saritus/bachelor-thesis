8106a7612769a3b5a6478fa16155bb37f5a4a90e
Sebastian Mischke <SebastianMischke@gmx.de>
2017-07-14 22:09:44 +0200

    Simply formatting

--- classification.py
+++ classification.py
@@ -111,8 +111,10 @@ def main():
     nb_epoch = 50
 
     checkpointer = ModelCheckpoint(filepath='models/weights.hdf5', verbose=1, save_best_only=True)
-    history = model.fit(x=X_train, y=Y_train, batch_size=batch_size, epochs=nb_epoch,
-                        verbose=2, shuffle=True, validation_split=0.1, callbacks=[checkpointer])
+    history = model.fit(x=X_train, y=Y_train,
+                        batch_size=batch_size, epochs=nb_epoch,
+                        verbose=2, shuffle=True,
+                        validation_split=0.20, callbacks=[checkpointer])
 
     # save weights of net
     # save_model(model, "models/first_try.json", "models/first_try.h5")
