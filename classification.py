84b8e5c42f35f538f9c69ee637312f7d2ae615d6
Sebastian Mischke <SebastianMischke@gmx.de>
2017-07-14 22:11:31 +0200

    Reinit net until start prediction is about near 0.5

--- classification.py
+++ classification.py
@@ -105,8 +105,17 @@ def load_model(file_architecture, file_weights=None):
 def main():
     (Coordinates), (X_train, Y_train) = load_csv("nwt-data/Schulen_Hauser_Dresden.csv")
 
-    model = create_net(X_train, Y_train)
-    # model = load_model("models/first_try.json", "models/first_try.h5")
+    import numpy
+    avg = 0
+    while avg < 0.2 or avg > 0.8:
+        model = create_net(X_train, Y_train)
+        # model = load_model("models/first_try.json", "models/first_try.h5")
+
+        prediction = model.predict(X_train)
+        print numpy.average(prediction)
+        avg = numpy.average(prediction)
+
+    print [round(nmb, 1) for nmb in prediction[:18].flatten()]
 
     ## Some model and data processing constants
     batch_size = 16
