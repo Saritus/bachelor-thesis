ef84ebe5c9d6bb0ba9de9ffcde0f49463c6d83b2
Sebastian Mischke <SebastianMischke@gmx.de>
2017-07-18 23:05:32 +0200

    Change weights output file

--- classification.py
+++ classification.py
@@ -132,7 +132,7 @@ def main():
     batch_size = 32
     nb_epoch = 100
 
-    checkpointer = ModelCheckpoint(filepath='models/cityDE.hdf5', verbose=1, save_best_only=True)
+    checkpointer = ModelCheckpoint(filepath='models/schoolsSA.hdf5', verbose=1, save_best_only=True)
     history = model.fit(x=X_train, y=Y_train,
                         batch_size=batch_size, epochs=nb_epoch,
                         verbose=1, shuffle=True,
