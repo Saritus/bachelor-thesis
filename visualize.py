51f8b9ca68694a30946748e2d4ff8ab90bad662f
Sebastian Mischke <SebastianMischke@gmx.de>
2017-07-20 02:30:51 +0200

    Dont show validation graphs

--- visualize.py
+++ visualize.py
@@ -2,7 +2,7 @@ def show_acc(history):
     # ACC VS VAL_ACC
     import matplotlib.pyplot as plt
     plt.plot(history.history['categorical_accuracy'])
-    plt.plot(history.history['val_categorical_accuracy'])
+    #plt.plot(history.history['val_categorical_accuracy'])
     plt.title('model accuracy ACC VS VAL_ACC')
     plt.ylabel('accuracy')
     plt.xlabel('epoch')
@@ -14,7 +14,7 @@ def show_loss(history):
     # LOSS VS VAL_LOSS
     import matplotlib.pyplot as plt
     plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
+    #plt.plot(history.history['val_loss'])
     plt.title('model loss LOSS vs VAL_LOSS')
     plt.ylabel('loss')
     plt.xlabel('epoch')
